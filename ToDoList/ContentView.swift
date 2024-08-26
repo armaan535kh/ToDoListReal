b807126b72132b7c4e6c23340394f428a04c3308
armaan535kh <kh.armaan535@gmail.com>
2024-08-26 04:58:10 -0700

    Created a + sign button to add list

--- ToDoList/ContentView.swift
+++ ToDoList/ContentView.swift
@@ -5,17 +5,28 @@
 //  Created by Armaan Khan  on 25/08/24.
 //
 
-import SwiftUI
+import SwiftUI 
 
 struct ContentView: View {
+    @State private var isClicked = false
+    @State private var isTemporary = false
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            Spacer()
+            Button(action: {
+                isClicked.toggle()
+                isTemporary = true
+                DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
+                    isTemporary = false
+                }
+            }) {
+                Image( systemName: "plus.circle.fill")
+                    .foregroundColor(isTemporary ? .blue :.gray)
+                    .font(.system(size: 50))
+                    .scaleEffect(isTemporary ? 1.2 : 1.0)
+            }
+            
         }
-        .padding()
     }
 }
 
