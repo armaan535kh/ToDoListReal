5f038399460ed34c87628daa57804a37795ae390
armaan535kh <kh.armaan535@gmail.com>
2024-08-27 20:30:02 -0700

    Final product

---
+++ ToDoList/AddView.swift
@@ -0,0 +1,18 @@
+//
+//  AddView.swift
+//  ToDoList
+//
+//  Created by Armaan Khan  on 26/08/24.
+//
+
+import SwiftUI
+
+struct AddView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddView()
+}
