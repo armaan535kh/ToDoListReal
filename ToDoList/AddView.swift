6e0766ed0d74500f534c466032d16c4face81de7
armaan535kh <kh.armaan535@gmail.com>
2024-08-27 20:32:54 -0700

    example

--- ToDoList/AddView.swift
+++ ToDoList/AddView.swift
@@ -8,11 +8,35 @@
 import SwiftUI
 
 struct AddView: View {
+    @Binding var items: [String]
+    @State private var text = ""
+    @Environment(\.presentationMode) var presentationMode
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+           
+            Form {
+                TextField("Add the item", text: $text)
+                    
+            }
+            .navigationTitle("Add Item")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button(action: {
+                        items.append(text)
+                        presentationMode.wrappedValue.dismiss()
+                       // text = ""
+                    }) {
+                        Text("Done")
+                    }
+                }
+            }
+                
+        }
+        
     }
 }
 
 #Preview {
-    AddView()
+    AddView(items: .constant([]))
 }
--- ToDoList/ContentView.swift
+++ ToDoList/ContentView.swift
@@ -10,23 +10,50 @@ import SwiftUI
 struct ContentView: View {
     @State private var isClicked = false
     @State private var isTemporary = false
+    @State private var showSheet = false
+    @State private var items: [String] = []
     var body: some View {
         NavigationStack {
+            List{
+                ForEach(items, id: \.self) { item in
+                    Text(item)
+                        .swipeActions{
+                            Button(action: {
+                                
+                                items.remove(at: items.firstIndex(of: item)!)
+                            }) {
+                                Image(systemName: "trash")
+                            }
+                        }
+                }
+                
+            }
+            
             Spacer()
             Button(action: {
                 isClicked.toggle()
                 isTemporary = true
-                DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
+                showSheet = true
+                DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
                     isTemporary = false
                 }
-            }) {
+            })
+            {
                 Image( systemName: "plus.circle.fill")
-                    .foregroundColor(isTemporary ? .blue :.gray)
+                    .foregroundColor(.gray)
                     .font(.system(size: 50))
                     .scaleEffect(isTemporary ? 1.2 : 1.0)
             }
+            .sheet(isPresented: $showSheet) {
+                AddView(items: $items)
+                   }
+            
+            .navigationTitle("To Do list")
+            .navigationBarTitleDisplayMode(.inline)
+            
             
         }
+       
     }
 }
 
